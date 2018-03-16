133c44e9dcdb61fb17f8dadfcbacd9fca661c7e7
denimar <denimar@gmail.com>
2018-03-15 21:52:40 -0300

    exampled implemented

--- gh-pages/App.jsx
+++ gh-pages/App.jsx
@@ -1,27 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import IndexPage from './IndexPage'
 import Examples from './examples/components/Examples'
-import { HashRouter, Switch, Route } from 'react-router-dom'
-
-class App extends React.Component {
-
-  render() {
-    return (
-      <div className='app-container'>
-        <Switch>
-          <Route exact path='/' component={ IndexPage } />
-          <Route path="/examples" component={ Examples } />
-        </Switch>
-      </div>
-    )
-  }
-
-}
 
 ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
+  <Examples />,
   document.getElementById('root')
 )
